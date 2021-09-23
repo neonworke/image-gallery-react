eb1e973d2e641c094d586ded88c9f96ea17f3377
neonworke <amanagrawal181@gmail.com>
2021-09-23 19:13:22 +0530

    UI built

---
+++ image-gallery-react/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import "tailwindcss/tailwind.css";
+
+function App() {
+  const [photoGalleryArray, updatePhotoGalleryArray] = useState([]);
+  useEffect(() => {
+    // API call for fetching images 
+    axios.get('https://picsum.photos/v2/list')
+      .then(function (response) {
+        // handle success
+        console.log("Data received",response);
+        updatePhotoGalleryArray(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="Header w-full m-10">
+      <h2 className="w-full font-bold text-xl">Photographer's Showcase</h2>
+      </div>
+      <div className="modal-container h-40 w-40">
+            <div>
+                <button>
+                <span class="material-icons">cancel</span>
+                </button>
+            </div>
+            <img src={photoGalleryArrayItem.download_url}/>
+        </div>
+      <div className="grid grid-cols-3 gap-10 ml-40">
+        {
+          photoGalleryArray.map((photoGalleryArrayItem, index) => {
+            return (
+              <div key={index} className="border-2 border-gray-300 h-56 w-72 shadow-lg rounded-b-lg" onClick={() => getImg(photoGalleryArrayItem.download_url)}>
+                <img src={photoGalleryArrayItem.download_url}
+                  alt={`image_${photoGalleryArrayItem.id}`}
+                  className="h-48 w-72"/>
+                <p className="pt-1 font-semibold">Image by: {photoGalleryArrayItem.author}</p>
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
